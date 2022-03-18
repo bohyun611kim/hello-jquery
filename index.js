197c25af9b86bd2b7544c20a413225ace2cc6a7e
kbh <bohyun611.kim@gmai.com>
2022-03-18 11:28:18 +0900

    dom : adding and removing element

---
+++ index.js
@@ -0,0 +1,7 @@
+// $('h1').addClass('big-title margin-50');
+//$('h1').text('Bye!');
+//$('button').html('<em>Hey button!</em>');
+
+console.log($('img').attr('src'));
+
+$('a').attr('href', 'https://www.yahoo.com');
